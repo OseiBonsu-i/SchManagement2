29046af90d4c97ed66e890bdc4fb387bef0a0d0d
Osei-Bonsu <ikebonsu9@gmail.com>
2022-04-04 16:35:22 +0000

    Initial Setup

--- routes/studentRoutes.js
+++ routes/studentRoutes.js
@@ -1,9 +1,6 @@
 const express = require('express')
 const router = express.Router()
-// const mysql = require('mysql2')
 const db = require('../config/db')
-// const pool = require('../config/db')
-// const studentControllers = require('../controllers/studentControllers')
 
 router.get('/show', (req, res) =>{
     res.render('students/show')
@@ -12,30 +9,6 @@ router.get('/show', (req, res) =>{
 router.get('/admission', (req, res) =>{
     res.render('students/admission')
 })
-
-// router.post('/', async (req, res) => {
-//     try {
-//         let firstname = req.body.firstname
-//         let lastname = req.body.lastname
-//         let age = req.body.age
-//         let sql = `INSERT INTO students(firstname, lastname, age) VALUES('${firstname}', '${lastname}', '${age}')`
-//         db.query(sql, function (err, result) {
-//             if (err) throw err
-//             console.log('record inserted')
-//             res.redirect('/')
-    
-//         })
-        
-//     } catch (err) {
-//         res.send(err)
-//     }
-
-
-
-
-// })
-
-// router.route('/:id').get(studentControllers.getstudentById)
 
 
 module.exports = router
